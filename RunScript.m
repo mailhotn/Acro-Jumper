8a874e9de5d634e03514f49a4cc7bad0cae9457f
Nadav Mailhot <mailhotn@gmail.com>
2017-08-17 15:58:21 +0300

    Addition For Submission

--- RunScript.m
+++ RunScript.m
@@ -26,7 +26,7 @@ while ~EndCond
     [tTime, tX, tTe, tXe,tIe] = ode45(@Sim.Derivative,[Time(end) inf], Xf, opt);
     Ie = [Ie; tIe]; Te = [Te; tTe]; %#ok
     X  = [X; tX]; Time = [Time; tTime]; %#ok
-    Xe = [Xe; tXe];
+    Xe = [Xe; tXe]; %#ok
     for ii = 1:length(tX(:,1))
         AJF = Sim.Mod.copy;
         AJF.tau = Sim.Con.calc_tau(tTime(ii));
@@ -45,9 +45,18 @@ while ~EndCond
         EndCond = 1;
     end
 end
+%% Get Distance
+LandEventInd = find(Ie == 4);                        % the index of the landing event
+if ~isempty(LandEventInd)
+    LandTime = Te(LandEventInd(1));                  % get the time of landing
+    LandTimeInd = find(abs(Time-LandTime) < 1e-6);   % find the timestep index of the land time
+    Pmin = min(X(LandTimeInd:end,1));                % find the minimal coordinate of P after landing time
+    d = min([Pmin, AJ.LandingQR]) - AJ.LiftOff;
+    disp(['Jump Length: ' num2str(d*100) ' centimeters'])
+end
 %% Animate simulation
-% load handel
-% sound(y,Fs)
+load handel
+sound(y,Fs)
 figure(2)
 % path = animatedline('color', 'b', 'linewidth', 2);
 for ii = 1:length(Time)-1
@@ -57,7 +66,7 @@ for ii = 1:length(Time)-1
 %     drawnow;
     pause(dt*11);
 end
-%%
+%% Draw Graphs For Report
 figure(3)
 tau = Time;
 for ii = 1:length(Time)
@@ -71,7 +80,7 @@ set(h,'Color',[0.3,0.3,0.3])
 xlabel('Time [sec]')
 ylabel('\tau [Nm]')
 
-a = figure(4)
+a = figure(4);
 plot([0,Time(end)],[-AJ.mu -AJ.mu; AJ.mu AJ.mu].',':r',Time, Ft./Fn,'Linewidth',1.3)
 ylim([-0.4,0.4])
 % title('Reaction Force Ratio')
@@ -80,14 +89,14 @@ ylabel('F_t/F_n')
 h = vline(Te);
 set(h,'Color',[0.3,0.3,0.3])
 
-b = figure(5)
+b = figure(5);
 plot(Time, Fn,[0,Time(end)],[0 0],':r','Linewidth',1.3)
 % title('Normal Reaction Force')
 xlabel('Time [sec]')
 ylabel('F_n [N]')
 h = vline(Te);
 set(h,'Color',[0.3,0.3,0.3])
-b.Position = a.Position
+b.Position = a.Position;
 
 figure(6)
 plot(Time, [X(:,1),X(:,3)],'Linewidth',1.3)
@@ -111,7 +120,6 @@ for ii =1:9
     AJs{ii} = AcroJumper;
     AJs{ii}.LineWidth = 0.005;
     AJs{ii}.link_width = 0.01;
-%     ,'LineWidth',0.01,'link_width',0.1);
     Render(AJs{ii},[],drawX(ii,:),1);
     hold on
 end
